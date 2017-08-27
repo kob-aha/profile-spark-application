ee00b38bcc7a85d7a77c1972b588e38d26abd234
Koby Aharon <kobyahron@gmail.com>
2017-08-27 14:34:12 +0300

    Fix issues in compose YAML file and add influxdb

--- influxdb/run-client.sh
+++ influxdb/run-client.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run --rm --link=influxdb -it influxdb influx -host influxdb
+SERVER_HOSTNAME=spark_influxdb_1
+
+docker run --rm --link=$SERVER_HOSTNAME:influxdb -it influxdb:1.3.4 influx -host influxdb
--- spark/run-driver.sh
+++ spark/run-driver.sh
@@ -1,9 +1,13 @@
 FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MASTER_CONTAINER_NAME=spark_spark-master_1
 MASTER_PORT=7077
+INFLUXDB_CONTAINER_NAME=spark_influxdb_1
+
+
+docker build -t=spark:2.0.0 $FOLDER
 
 docker run --rm -it -p 8088:8088 -p 8042:8042 -p 4041:4040 \
   -v $FOLDER/graph:/tmp/graph \
-  --link ${MASTER_CONTAINER_NAME}:spark-master --link influxdb \
+  --link ${MASTER_CONTAINER_NAME}:spark-master --link $INFLUXDB_CONTAINER_NAME:influxdb \
   --name driver -h driver spark:2.0.0 \
   spark-shell --master spark://spark-master:${MASTER_PORT}
